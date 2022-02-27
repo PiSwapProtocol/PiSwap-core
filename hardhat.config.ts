1c652bb02626328046c8cb10067a068775fbf9e2
gretzke <daniel@gretzke.de>
2022-02-27 04:57:20 +0100

    add config for CI workflow

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,70 +8,111 @@ import 'solidity-coverage';
 require('dotenv').config();
 require('hardhat-contract-sizer');
 
-const config: HardhatUserConfig = {
-  defaultNetwork: 'hardhat',
-  networks: {
-    rinkeby: {
-      url: 'https://rinkeby.infura.io/v3/' + process.env.INFURA_TOKEN,
-      accounts: {
-        mnemonic: process.env.MNEMONIC as string,
-      },
-    },
-    goerli: {
-      url: 'https://goerli.infura.io/v3/' + process.env.INFURA_TOKEN,
-      accounts: {
-        mnemonic: process.env.MNEMONIC as string,
-      },
-    },
-    mumbai: {
-      url: 'https://polygon-mumbai.infura.io/v3/' + process.env.INFURA_TOKEN,
-      accounts: {
-        mnemonic: process.env.MNEMONIC as string,
-      },
-      gasPrice: 8000000000,
-    },
-    hardhat: {},
-  },
-  solidity: {
-    compilers: [
-      {
-        version: '0.8.11',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
+const config: HardhatUserConfig =
+  process.env.INFURA_TOKEN === 'dev'
+    ? {
+        defaultNetwork: 'hardhat',
+        networks: {
+          rinkeby: {
+            url: 'https://rinkeby.infura.io/v3/' + process.env.INFURA_TOKEN,
+            accounts: {
+              mnemonic: process.env.MNEMONIC as string,
+            },
           },
+          goerli: {
+            url: 'https://goerli.infura.io/v3/' + process.env.INFURA_TOKEN,
+            accounts: {
+              mnemonic: process.env.MNEMONIC as string,
+            },
+          },
+          mumbai: {
+            url: 'https://polygon-mumbai.infura.io/v3/' + process.env.INFURA_TOKEN,
+            accounts: {
+              mnemonic: process.env.MNEMONIC as string,
+            },
+            gasPrice: 8000000000,
+          },
+          hardhat: {},
         },
-      },
-    ],
-  },
-  gasReporter: {
-    currency: 'USD',
-    gasPrice: 100,
-    enabled: process.env.REPORT_GAS === 'true',
-    excludeContracts: [
-      'ProxyTest',
-      'MockERC165',
-      'ERC721',
-      'MockERC721',
-      'MockERC721Royalty',
-      'MockERC1155',
-      'MockERC1155Royalty',
-      'UpgradeTestA',
-      'UpgradeTestB',
-      'WETH9',
-    ],
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_TOKEN,
-  },
-  mocha: {
-    timeout: 100000,
-  },
-  typechain: {
-    alwaysGenerateOverloads: true,
-  },
-};
+        solidity: {
+          compilers: [
+            {
+              version: '0.8.11',
+              settings: {
+                optimizer: {
+                  enabled: true,
+                  runs: 200,
+                },
+              },
+            },
+          ],
+        },
+        gasReporter: {
+          currency: 'USD',
+          gasPrice: 100,
+          enabled: process.env.REPORT_GAS === 'true',
+          excludeContracts: [
+            'ProxyTest',
+            'MockERC165',
+            'ERC721',
+            'MockERC721',
+            'MockERC721Royalty',
+            'MockERC1155',
+            'MockERC1155Royalty',
+            'UpgradeTestA',
+            'UpgradeTestB',
+            'WETH9',
+          ],
+        },
+        etherscan: {
+          apiKey: process.env.ETHERSCAN_TOKEN,
+        },
+        mocha: {
+          timeout: 100000,
+        },
+        typechain: {
+          alwaysGenerateOverloads: true,
+        },
+      }
+    : {
+        defaultNetwork: 'hardhat',
+        networks: {
+          hardhat: {},
+        },
+        solidity: {
+          compilers: [
+            {
+              version: '0.8.11',
+              settings: {
+                optimizer: {
+                  enabled: true,
+                  runs: 200,
+                },
+              },
+            },
+          ],
+        },
+        gasReporter: {
+          currency: 'USD',
+          gasPrice: 100,
+          enabled: true,
+          excludeContracts: [
+            'ProxyTest',
+            'MockERC165',
+            'ERC721',
+            'MockERC721',
+            'MockERC721Royalty',
+            'MockERC1155',
+            'MockERC1155Royalty',
+            'UpgradeTestA',
+            'UpgradeTestB',
+            'WETH9',
+          ],
+        },
+        mocha: {
+          timeout: 100000,
+        },
+      };
 
 task('accounts', 'Prints the list of accounts', async (args, hre) => {
   const accounts = await hre.ethers.getSigners();
